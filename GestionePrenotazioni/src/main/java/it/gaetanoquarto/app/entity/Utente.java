dc36b5504d858c93b44745abd7e3406254f90a83
gaetanoquarto <gaetanoqquarto@gmail.com>
2023-02-15 16:55:36 +0100

    commit

---
+++ GestionePrenotazioni/src/main/java/it/gaetanoquarto/app/entity/Utente.java
@@ -0,0 +1,44 @@
+package it.gaetanoquarto.app.entity;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.springframework.context.annotation.Scope;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "utenti")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Scope("prototype")
+@Builder
+public class Utente {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int utente_id;
+	private String username;
+	private String nome;
+	private String email;
+	
+	@OneToMany(mappedBy = "utente")
+	@JsonIgnoreProperties("utente")
+	private Set<Prenotazione> prenotazioni;
+}
